70bed16aa6e15ff50992c71aab4e31ae7f4dc8d1
Nam Anh <namanht96@gmail.com>
2022-01-05 23:00:18 +0700

    Revert "middleware"
    This reverts commit 377799f8909253d3455ba595368ff5c88fbfe7a0.

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
+const app = express()
 const route = require('./routes')
 const db = require('./config/database/index')
-const bodyParser = require('body-parser')
-const jwt = require('jsonwebtoken')
-const cookieParser = require('cookie-parser')
-const methodOverride = require('method-override')
-const app = express()
 
-//public file
+
 app.use(express.static(path.join(__dirname, 'public')))
-
-//morgan
 app.use(morgan('combined'))
 
 //views engine
@@ -21,14 +15,7 @@ app.engine('handlebars', handlebars())
 app.set('view engine', 'handlebars')
 app.set('views',path.join(__dirname, 'resources/views'))
 
-//body parser
-app.use(bodyParser.urlencoded({extended: true}))
-
-//cookies
-app.use(cookieParser())
-
-//use other method (PUT, DELETE)
-app.use(methodOverride('_method'))
+const port = 5000
 
 //routing
 route(app)
@@ -37,8 +24,6 @@ route(app)
 db.connect()
 
 //listen
-const port = 5000
-
 app.listen(port, function(error){
     if (error) {
         console.log("Something went wrong");
